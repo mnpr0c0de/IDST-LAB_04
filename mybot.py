e30b35e981d4710082bd07868b2da0f64c904b03
Nour Mihai-Cosmin <mihai_cosmin.nour@stud.acs.upb.ro>
2022-12-15 13:23:16 +0200

    BOT
    Signed-off-by: Nour Mihai-Cosmin <mihai_cosmin.nour@stud.acs.upb.ro>

--- mybot.py
+++ mybot.py
@@ -88,3 +88,16 @@ if __name__ == '__main__':
         exit(-1)
     # launch bot (blocking operation)
     bot.run(os.environ['BOT_TOKEN'])
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--token", help="Some token",
+                    action="store_true")
+args = parser.parse_args()
+
+# Copyright 2022 Nour Mihai-Cosmin
+#     This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. 
